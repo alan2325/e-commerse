390e8b49ca0a906efac5d1bada7388f2c495278b
alan2325 <alalkumar06@gmail.com>
2024-12-02 23:55:28 +0530

    orderdetails

--- project/app/models.py
+++ project/app/models.py
@@ -35,6 +35,10 @@ class cart(models.Model):
 
     def __str__(self):
         return self.user.name +' '+self.product.name
+    
+    def total_price(self):
+        return self.quantity * self.product.price
+    
 
  
 class buy(models.Model):
